fc1bd9200275841f17b38f78bac780ad138d1cc9
Ian Maffett <ian@appmobi.com>
2012-06-05 08:47:06 -0400

    Minor fixes/enhancements for new ajax based panels

--- ui/src/jq.ui.js
+++ ui/src/jq.ui.js
@@ -618,10 +618,19 @@
             var el = $am(id);
             if (!el)
                 return;
+            
+            var newDiv = document.createElement("div");
+	        newDiv.innerHTML = content;
+	        if($(newDiv).children('.panel') && $(newDiv).children('.panel').length > 0) newDiv = $(newDiv).children('.panel').get();
+	            
+             
+            
             if (el.getAttribute("scrolling") && el.getAttribute("scrolling").toLowerCase() == "no")
-                el.innerHTML = content;
+               el.innerHTML = newDiv.innerHTML;
             else
-                el.childNodes[0].innerHTML = content;
+                el.childNodes[0].innerHTML = newDiv.innerHTML;
+                
+            if($(newDiv).title) el.title = $(newDiv).title;
         },
         /**
          * Dynamically create a new panel on the fly.  It wires events, creates the scroller, applies Android fixes, etc.
@@ -636,10 +645,13 @@
         addContentDiv: function(el, content, title, refresh, refreshFunc) {
             var myEl = $am(el);
             if (!myEl) {
-                var newDiv = document.createElement("div");
-                newDiv.id = el;
-                newDiv.title = title;
-                newDiv.innerHTML = content;
+            	var newDiv = document.createElement("div");
+	            newDiv.innerHTML = content;
+	            if($(newDiv).children('.panel') && $(newDiv).children('.panel').length > 0) newDiv = $(newDiv).children('.panel').get();
+	            
+				if(!newDiv.title&&title) newDiv.title = title;
+				var newId = (newDiv.id)? newDiv.id : el; //figure out the new id - either the id from the loaded div.panel or the crc32 hash
+				newDiv.id = newId;
             } else {
                 newDiv = myEl;
             }
@@ -649,7 +661,7 @@
             myEl = null;
             that.addDivAndScroll(newDiv, refresh, refreshFunc);
             newDiv = null;
-            return;
+            return newId;
         },
         /**
          *  Takes a div and sets up scrolling for it..
@@ -890,7 +902,7 @@
 
                     //ajax div already exists.  Let's see if we should be refreshing it.
                     loadAjax = false;
-                    if (anchor.getAttribute("data-refresh-ajax") === 'true' || (anchor.refresh && anchor.refresh === true)||this.isAjaxApp) {
+                    if ((anchor && anchor.getAttribute("data-refresh-ajax") === 'true') || (anchor && anchor.refresh && anchor.refresh === true)||this.isAjaxApp) {
                         loadAjax = true;
                     } else
                         target = "#" + urlHash;
@@ -915,9 +927,8 @@
                         //Here we check to see if we are retaining the div, if so update it
                         if ($am(urlHash) !== undefined) {
                             that.updateContentDiv(urlHash, xmlhttp.responseText);
-                            $am(urlHash).title = anchor.title ? anchor.title : target;
-                        } else if (anchor.getAttribute("data-persist-ajax")||that.isAjaxApp) {
-                            
+                            if(!$am(urlHash).title) $am(urlHash).title = anchor.title ? anchor.title : target;
+                        } else if (that.isAjaxApp || anchor.getAttribute("data-persist-ajax")) {    
                             var refresh = (anchor.getAttribute("data-pull-scroller") === 'true') ? true : false;
                             refreshFunction = refresh ? 
                             function() {
@@ -925,8 +936,7 @@
                                 that.loadContent(target, newTab, back, transition, anchor);
                                 anchor.refresh = false;
                             } : null
-                            that.addContentDiv(urlHash, xmlhttp.responseText, refresh, refreshFunction);
-                            $am(urlHash).title = anchor.title ? anchor.title : target;
+                            that.addContentDiv(urlHash, xmlhttp.responseText,anchor.title ? anchor.title : target, refresh, refreshFunction);
                         } else {
                             that.updateContentDiv("jQui_ajax", xmlhttp.responseText);
                             $am("jQui_ajax").title = anchor.title ? anchor.title : target;
@@ -940,8 +950,7 @@
                         that.parseScriptTags(div);
                         if (doReturn)
                             return;
-                        
-                        return that.loadContent("#" + urlHash);
+                        return that.loadContent("#" + urlHash, false, back, null);
                     
                     }
                 };
@@ -1232,9 +1241,8 @@
                 //window.setTimeout(function() {
                 var loadFirstDiv=function(){
                     //activeDiv = firstDiv;
-                    if (defaultHash.length > 0 && that.loadDefaultHash&&defaultHash!=("#"+that.firstDiv.id))
+                    if (defaultHash.length > 0 && that.loadDefaultHash&&defaultHash!=("#"+that.firstDiv.id)&&$(defaultHash).length>0)
                     {
-                        
                         that.activeDiv=$(defaultHash).get();
                         jq("#header #backButton").css("visibility","visible");
                         that.setBackButtonText(that.activeDiv.title)
@@ -1606,15 +1614,12 @@
             
             var mytransition = theTarget.getAttribute("data-transition");
             var resetHistory = theTarget.getAttribute("data-resetHistory");
-            
+            var back = (theTarget.getAttribute("data-reverse") && theTarget.getAttribute("data-reverse") == 'true')?true:false;
             resetHistory = resetHistory && resetHistory.toLowerCase() == "true" ? true : false;
             
             var href = theTarget.hash.length > 0 ? theTarget.hash : theTarget.href;
-            //if (theTarget.onclick && !jq.os.desktop) {
-              //  theTarget.onclick();
-            //}
-            jq.ui.loadContent(href, resetHistory, 0, mytransition, theTarget);
-            
+            jq.ui.loadContent(href, resetHistory, back, mytransition, theTarget);
+
             return true;
         }
     }
