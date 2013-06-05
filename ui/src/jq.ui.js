7333d64c64de59a0fa7ff02633333c5e73235e18
Phil. Austermann <phil@patient-radio.de>
2013-06-05 08:20:16 +0200

    # same titlebar typo fix for src/jq.ui.js

--- ui/src/jq.ui.js
+++ ui/src/jq.ui.js
@@ -82,7 +82,7 @@
         showLoading: true,
         loadContentQueue: [],
         isAppMobi: false,
-        titlebar: "",
+        titleBar: "",
         navbar: "",
         header: "",
         viewportContainer: "",
