b62576831bdd4cbb1d6e495b65df54746749598e
Ian Maffett <ian.maffett@intel.com>
2013-06-27 13:27:15 -0400

    Fix for users doing extreme swiping on the screen.

--- plugins/af.slidemenu.js
+++ plugins/af.slidemenu.js
@@ -10,6 +10,7 @@
         showHide = false;
     $.ui.slideSideMenu = true;
     $.ui.fixedSideMenuWidth = 768;
+    var keepOpen=false;
     $.ui.ready(function() {
         //  $("head").append("<style>#afui #menu {display:block !important}</style>");
         if ($.os.ie) return; //ie has the menu at the bottom
@@ -29,8 +30,10 @@
             startY = e.touches[0].pageY;
 
             checking = false;
+            doMenu=false;
+            keepOpen=false;
             if (window.innerWidth >= $.ui.fixedSideMenuWidth)
-                doMenu = false;
+                doMenu = false,keepOpen=true;
             else
                 doMenu = true;
             max = $("#menu").width();
@@ -38,22 +41,28 @@
         });
         $("#afui").bind("touchmove", function(e) {
 
-            if (!$.ui.slideSideMenu) return true;
+            
+            if (!$.ui.slideSideMenu||keepOpen) return true;
             if (!checking) {
                 checking = true;
+                doMenu=false;
                 return true;
             }
+            else 
+                doMenu=true;
+             if(!doMenu) return;
 
             var dx = e.touches[0].pageX;
             var dy = e.touches[0].pageY;
-            if (!menuState && dx < startX) return;
-            else if (menuState && dx > startX) return;
-            if (!doMenu || Math.abs(dy - startY) > Math.abs(dx - startX)) {
+            //if (!menuState && dx < startX) return;
+            //else if (menuState && dx > startX) return;
+            if (Math.abs(dy - startY) > Math.abs(dx - startX)) {
                 doMenu = false;
                 return true;
-            }
-            $menu.show();
+            }            
+            console.log(dx,max);
             if (dx > max) return true;
+            console.log(dy,startY,dx,startX);
             showHide = dx - startX > 0 ? 2 : false;
             var thePlace = Math.abs(dx - startX);
 
@@ -70,13 +79,13 @@
 
         });
         $("#afui").bind("touchend", function(e) {
-
-            if (doMenu && checking) {
-                if (showHide) $menu.hide();
+            
+            if (doMenu && checking&&!keepOpen) {
                 $.ui.toggleSideMenu(showHide, null, transTime);
             }
             checking = false;
             doMenu = false;
+            keepOpen=false;
         });
     });
 })(af);
