965ec8cd3e033ca41961e479271314f11d9fcc44
rylan <rylan@testdev02.lostcherry.com>
2013-07-23 22:50:35 -0700

    fix href having hashes inside and they double up

--- ui/src/appframework.ui.js
+++ ui/src/appframework.ui.js
@@ -8,7 +8,7 @@
     "use strict";
 
     var hasLaunched = false;
-    var startPath = window.location.href;
+    var startPath = window.location.href.split('#', 1)[0];
     var defaultHash = window.location.hash;
     var previousTarget = defaultHash;
     var ui = function() {
