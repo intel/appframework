7267fff145eb0361981c00bb5e92023ea3829f00
Elliot Smith <elliot.smith@intel.com>
2013-08-28 09:43:30 +0100

    Expose chai's expect() function globally
    This enables more natural syntax for test cases where
    we're testing for undefined/null.

--- test/chai.helper.js
+++ test/chai.helper.js
@@ -3,5 +3,6 @@ var chai = require("chai")
 // turn on stack traces for errors thrown during chai assertions
 chai.Assertion.includeStack = true;
 
-// make should API globally available
+// make API globally available
 global.should = chai.should();
+global.expect = chai.expect;
