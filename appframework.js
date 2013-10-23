59768a161811aae4f3b6c6d68737e0cb7d2509b7
Ian Maffett <ian.maffett@intel.com>
2013-10-23 08:32:38 -0400

    fix ios7 UA sniffing

--- appframework.js
+++ appframework.js
@@ -1926,7 +1926,7 @@ if (!window.af || typeof(af) !== "function") {
             $.os.androidICS = $.os.android && userAgent.match(/(Android)\s4/) ? true : false;
             $.os.ipad = userAgent.match(/(iPad).*OS\s([\d_]+)/) ? true : false;
             $.os.iphone = !$.os.ipad && userAgent.match(/(iPhone\sOS)\s([\d_]+)/) ? true : false;
-            $.os.ios7 = userAgent.match(/(iPhone\sOS)\s([7_]+)/) ? true : false;
+            $.os.ios7 = ($.os.ipad||$.os.iphone)&&userAgent.match(/7_/) ? true : false;
             $.os.webos = userAgent.match(/(webOS|hpwOS)[\s\/]([\d.]+)/) ? true : false;
             $.os.touchpad = $.os.webos && userAgent.match(/TouchPad/) ? true : false;
             $.os.ios = $.os.ipad || $.os.iphone;
