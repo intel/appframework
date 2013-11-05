9d0d77eb7f5cc5ca70d0b21c98d88aa8d5a9b022
Rakshith <rakf1@hotmail.com>
2013-11-05 13:58:23 -0800

    fixed sticky slidemenu
    removed doMenu = false in "if (Math.abs(dy - startY) > Math.abs(dx - startX))" -> this was leaving the panel stuck in between, without this it will snap to either open or close. 
    
    removed "if (dx > max) return true;" -> this was only allowing panel swipe starting within the first 256px (width of sidemenu).
    
    if (dx-startX > max) return true; -> will make sure that panel will not slide out more that the width of side menu
    
    if (startX-dx > max) return true; -> will make sure that panel will not slide in more that the width of side menu

--- plugins/af.slidemenu.js
+++ plugins/af.slidemenu.js
@@ -57,12 +57,10 @@
             dy = e.touches[0].pageY;
             if (!menuState && dx < startX) return;
             else if (menuState && dx > startX) return;
-            if (Math.abs(dy - startY) > Math.abs(dx - startX)) {
-                doMenu = false;
-                return true;
-            }            
+            if (Math.abs(dy - startY) > Math.abs(dx - startX)) return true;
             
-            if (dx > max) return true;
+            if (dx-startX > max) return true;
+            if (startX-dx > max) return true;
             
             showHide = dx - startX > 0 ? 2 : false;
             var thePlace = Math.abs(dx - startX);
