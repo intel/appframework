03ca939a0f6fe86334d5bbfe1d281b54ebb48e6c
Ian Maffett <ian.maffett@intel.com>
2013-12-06 11:12:07 -0500

    add back element=null from trying to fix $().one

--- appframework.js
+++ appframework.js
@@ -2223,7 +2223,7 @@ if (!window.af || typeof(af) !== "function") {
                 set.push(handler);
                 element.addEventListener(handler.e, proxyfn, false);
             });
-            //element=null;
+            element=null;
         }
 
         /**
