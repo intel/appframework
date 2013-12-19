d1a2056213a121d0e58015f8caa6906b67427d65
Ian Maffett <ian.maffett@intel.com>
2013-12-19 15:18:41 -0500

    rebuild min files.

--- ui/appframework.ui.js
+++ ui/appframework.ui.js
@@ -4036,7 +4036,7 @@
          */
         setBackButtonText: function(text) {
             if(this._currentHeaderID!=="defaultHeader") return;
-            if (this.trimBackButtonText)
+            if (this.trimBackButtonText && text.length >= 7)
                 text = text.substring(0, 5) + "...";
             if (this.backButtonText.length > 0) $.query("#header header:not(.ignore) #backButton").html(this.backButtonText);
             else $.query("#header header:not(.ignore)  #backButton").html(text);
@@ -5273,6 +5273,221 @@
     }
 })(af);
 
+(function ($ui) {
+
+    /**
+     * Initiate a sliding transition.  This is a sample to show how transitions are implemented.  These are registered in $ui.availableTransitions and take in three parameters.
+     * @param {Object} previous panel
+     * @param {Object} current panel
+     * @param {Boolean} go back
+     * @title $ui.slideTransition(previousPanel,currentPanel,goBack);
+     */
+    function slideTransition(oldDiv, currDiv, back) {
+
+        oldDiv.style.display = "block";
+        currDiv.style.display = "block";
+        var that = this;
+        if (back) {
+            that.css3animate(oldDiv, {
+                x: "0%",
+                y: "0%",
+                complete: function () {
+                    that.css3animate(oldDiv, {
+                        x: "100%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    }).link(currDiv, {
+                        x: "0%",
+                        time: $ui.transitionTime
+                    });
+                }
+            }).link(currDiv, {
+                x: "-100%",
+                y: "0%"
+            });
+        } else {
+            that.css3animate(oldDiv, {
+                x: "0%",
+                y: "0%",
+                complete: function () {
+                    that.css3animate(oldDiv, {
+                        x: "-100%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    }).link(currDiv, {
+                        x: "0%",
+                        time: $ui.transitionTime
+                    });
+                }
+            }).link(currDiv, {
+                x: "100%",
+                y: "0%"
+            });
+        }
+    }
+    $ui.availableTransitions.slide = slideTransition;
+    $ui.availableTransitions['default'] = slideTransition;
+})(af.ui);
+(function ($ui) {
+
+    function slideUpTransition(oldDiv, currDiv, back) {
+        oldDiv.style.display = "block";
+        currDiv.style.display = "block";
+        var that = this;
+        if (back) {
+            oldDiv.style.zIndex = 2;
+            currDiv.style.zIndex = 1;
+            that.css3animate(oldDiv, {
+                y: "0%",
+                x: "0%",
+                complete: function () {
+                    that.css3animate(oldDiv, {
+                        y: "100%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    });
+                }
+            });
+        } else {
+            oldDiv.style.zIndex = 1;
+            currDiv.style.zIndex = 2;
+            that.css3animate(currDiv, {
+                y: "100%",
+                x: "0%",
+                time:"10ms",
+                complete: function () {
+                    that.css3animate(currDiv, {
+                        y: "0%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    });
+                }
+            });
+        }
+    }
+    $ui.availableTransitions.up = slideUpTransition;
+})(af.ui);
+(function ($ui) {
+
+    function slideDownTransition(oldDiv, currDiv, back) {
+        oldDiv.style.display = "block";
+        currDiv.style.display = "block";
+        var that = this;
+        if (back) {
+            oldDiv.style.zIndex = 2;
+            currDiv.style.zIndex = 1;
+            that.css3animate(oldDiv, {
+                y: "0%",
+                x: "0%",
+                complete: function () {
+                    that.css3animate(oldDiv, {
+                        y: "-100%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    });
+                }
+            });
+        } else {
+            oldDiv.style.zIndex = 1;
+            currDiv.style.zIndex = 2;
+            that.css3animate(currDiv, {
+                y: "-100%",
+                x: "0%",
+                time:"10ms",
+                complete: function () {
+                    that.css3animate(currDiv, {
+                        y: "0%",
+                        time: $ui.transitionTime,
+                        complete: function () {
+                            that.finishTransition(oldDiv, currDiv);
+                        }
+                    });
+                }
+            });
+        }
+    }
+    $ui.availableTransitions.down = slideDownTransition;
+})(af.ui);
+(function ($ui) {
+
+    function popTransition(oldDiv, currDiv, back) {
+        oldDiv.style.display = "block";
+        currDiv.style.display = "block";
+        var that = this;
+        if (back) {
+            currDiv.style.zIndex = 1;
+            oldDiv.style.zIndex = 2;
+            that.clearAnimations(currDiv);
+            that.css3animate(oldDiv, {
+                x:"0%",
+                time: $ui.transitionTime,
+                opacity: 0.1,
+                scale: 0.2,
+                origin:"50% 50%",
+                complete: function (canceled) {
+                    if (canceled) {
+                        that.finishTransition(oldDiv);
+                        return;
+                    }
+
+                    that.css3animate(oldDiv, {
+                        x: "-100%",
+                        complete: function () {
+                            that.finishTransition(oldDiv);
+                        }
+                    });
+                    currDiv.style.zIndex = 2;
+                    oldDiv.style.zIndex = 1;
+                }
+            });
+        } else {
+            oldDiv.style.zIndex = 1;
+            currDiv.style.zIndex = 2;
+            that.css3animate(currDiv, {
+                x: "0%",
+                scale: 0.2,
+                origin: "50%" + " 50%",
+                opacity: 0.1,
+                time:"0ms",
+                complete: function () {
+                    that.css3animate(currDiv, {
+                        x: "0%",
+                        time: $ui.transitionTime,
+                        scale: 1,
+                        opacity: 1,
+                        origin: "0%" + " 0%",
+                        complete: function (canceled) {
+                            if (canceled) {
+                                that.finishTransition(oldDiv, currDiv);
+                                return;
+                            }
+
+                            that.clearAnimations(currDiv);
+                            that.css3animate(oldDiv, {
+                                x: "100%",
+                                y: 0,
+                                complete: function () {
+                                    that.finishTransition(oldDiv);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+    $ui.availableTransitions.pop = popTransition;
+})(af.ui);
 (function($ui){
         function fadeTransition (oldDiv, currDiv, back) {
             oldDiv.style.display = "block";
@@ -5424,222 +5639,6 @@
     }
     $ui.availableTransitions.flip = flipTransition;
 })(af.ui);
-(function ($ui) {
-
-    function popTransition(oldDiv, currDiv, back) {
-        oldDiv.style.display = "block";
-        currDiv.style.display = "block";
-        var that = this;
-        if (back) {
-            currDiv.style.zIndex = 1;
-            oldDiv.style.zIndex = 2;
-            that.clearAnimations(currDiv);
-            that.css3animate(oldDiv, {
-                x:"0%",
-                time: $ui.transitionTime,
-                opacity: 0.1,
-                scale: 0.2,
-                origin:"50% 50%",
-                complete: function (canceled) {
-                    if (canceled) {
-                        that.finishTransition(oldDiv);
-                        return;
-                    }
-
-                    that.css3animate(oldDiv, {
-                        x: "-100%",
-                        complete: function () {
-                            that.finishTransition(oldDiv);
-                        }
-                    });
-                    currDiv.style.zIndex = 2;
-                    oldDiv.style.zIndex = 1;
-                }
-            });
-        } else {
-            oldDiv.style.zIndex = 1;
-            currDiv.style.zIndex = 2;
-            that.css3animate(currDiv, {
-                x: "0%",
-                scale: 0.2,
-                origin: "50%" + " 50%",
-                opacity: 0.1,
-                time:"0ms",
-                complete: function () {
-                    that.css3animate(currDiv, {
-                        x: "0%",
-                        time: $ui.transitionTime,
-                        scale: 1,
-                        opacity: 1,
-                        origin: "0%" + " 0%",
-                        complete: function (canceled) {
-                            if (canceled) {
-                                that.finishTransition(oldDiv, currDiv);
-                                return;
-                            }
-
-                            that.clearAnimations(currDiv);
-                            that.css3animate(oldDiv, {
-                                x: "100%",
-                                y: 0,
-                                complete: function () {
-                                    that.finishTransition(oldDiv);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
-    }
-    $ui.availableTransitions.pop = popTransition;
-})(af.ui);
-(function ($ui) {
-
-    /**
-     * Initiate a sliding transition.  This is a sample to show how transitions are implemented.  These are registered in $ui.availableTransitions and take in three parameters.
-     * @param {Object} previous panel
-     * @param {Object} current panel
-     * @param {Boolean} go back
-     * @title $ui.slideTransition(previousPanel,currentPanel,goBack);
-     */
-    function slideTransition(oldDiv, currDiv, back) {
-
-        oldDiv.style.display = "block";
-        currDiv.style.display = "block";
-        var that = this;
-        if (back) {
-            that.css3animate(oldDiv, {
-                x: "0%",
-                y: "0%",
-                complete: function () {
-                    that.css3animate(oldDiv, {
-                        x: "100%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    }).link(currDiv, {
-                        x: "0%",
-                        time: $ui.transitionTime
-                    });
-                }
-            }).link(currDiv, {
-                x: "-100%",
-                y: "0%"
-            });
-        } else {
-            that.css3animate(oldDiv, {
-                x: "0%",
-                y: "0%",
-                complete: function () {
-                    that.css3animate(oldDiv, {
-                        x: "-100%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    }).link(currDiv, {
-                        x: "0%",
-                        time: $ui.transitionTime
-                    });
-                }
-            }).link(currDiv, {
-                x: "100%",
-                y: "0%"
-            });
-        }
-    }
-    $ui.availableTransitions.slide = slideTransition;
-    $ui.availableTransitions['default'] = slideTransition;
-})(af.ui);
-(function ($ui) {
-
-    function slideDownTransition(oldDiv, currDiv, back) {
-        oldDiv.style.display = "block";
-        currDiv.style.display = "block";
-        var that = this;
-        if (back) {
-            oldDiv.style.zIndex = 2;
-            currDiv.style.zIndex = 1;
-            that.css3animate(oldDiv, {
-                y: "0%",
-                x: "0%",
-                complete: function () {
-                    that.css3animate(oldDiv, {
-                        y: "-100%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    });
-                }
-            });
-        } else {
-            oldDiv.style.zIndex = 1;
-            currDiv.style.zIndex = 2;
-            that.css3animate(currDiv, {
-                y: "-100%",
-                x: "0%",
-                time:"10ms",
-                complete: function () {
-                    that.css3animate(currDiv, {
-                        y: "0%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    });
-                }
-            });
-        }
-    }
-    $ui.availableTransitions.down = slideDownTransition;
-})(af.ui);
-(function ($ui) {
-
-    function slideUpTransition(oldDiv, currDiv, back) {
-        oldDiv.style.display = "block";
-        currDiv.style.display = "block";
-        var that = this;
-        if (back) {
-            oldDiv.style.zIndex = 2;
-            currDiv.style.zIndex = 1;
-            that.css3animate(oldDiv, {
-                y: "0%",
-                x: "0%",
-                complete: function () {
-                    that.css3animate(oldDiv, {
-                        y: "100%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    });
-                }
-            });
-        } else {
-            oldDiv.style.zIndex = 1;
-            currDiv.style.zIndex = 2;
-            that.css3animate(currDiv, {
-                y: "100%",
-                x: "0%",
-                time:"10ms",
-                complete: function () {
-                    that.css3animate(currDiv, {
-                        y: "0%",
-                        time: $ui.transitionTime,
-                        complete: function () {
-                            that.finishTransition(oldDiv, currDiv);
-                        }
-                    });
-                }
-            });
-        }
-    }
-    $ui.availableTransitions.up = slideUpTransition;
-})(af.ui);
-
 /**
  * af.8tiles - Provides a WP8 theme and handles showing the menu
  * Copyright 2012 - Intel
